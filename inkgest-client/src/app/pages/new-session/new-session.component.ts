13c7869b8db80b8adbdf116ff0d59a9869132508
diego skieresz <diegoskieresz@gmail.com>
2023-06-08 18:55:20 -0300

    Adjust the addMaterial and added getMaxQuantity in new-session.component.ts

--- inkgest-client/src/app/pages/new-session/new-session.component.ts
+++ inkgest-client/src/app/pages/new-session/new-session.component.ts
@@ -9,54 +9,59 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class NewSessionComponent {
 
   clients: any[] = [
-    {'name': 'Allan', 'information': '1'},
-    {'name': 'Diego', 'information': '12'},
-    {'name': 'Alvaro', 'information': '123'},
-    {'name': 'Allana', 'information': ''},
-  ]
+    { 'name': 'Allan', 'information': 'Alergia a stress' },
+    { 'name': 'Diego', 'information': 'Alergia a zinco' },
+    { 'name': 'Alvaro', 'information': 'Hepatite' },
+    { 'name': 'Allana', 'information': 'Diabetes' },
+  ];
 
   inventoryList: any[] = [
     {
       "_id": 1,
       "category": "Agulhas",
       "subcategory": "RL",
-      "description": "Agulha muito bonita e muita linda e muita errado",
+      "description": "Agulha traçado 3RL",
+      "quantity": 10
     },
     {
       "_id": 3,
       "category": "Agulhas",
-      "subcategory": "RL",
-      "description": "Agulha bonita",
+      "subcategory": "RS",
+      "description": "Agulha sombra 9RS",
+      "quantity": 5
     },
     {
       "_id": 4,
       "category": "Tintas",
-      "subcategory": "RL",
-      "description": "Agulha bonita",
+      "subcategory": "Preta",
+      "description": "Eletric Ink Tribal",
+      "quantity": 8
     },
     {
       "_id": 2,
       "category": "Descartáveis",
       "subcategory": "RL",
       "description": "Cartucho de agulhas para traçado",
+      "quantity": 3
     }
-  ]
+  ];
 
-  inv: any
+  inv: FormArray<any> = new FormArray<any>([]);
+
   formGroup!: FormGroup;
   filteredClients!: any[];
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
     this.formGroup = new FormGroup({
-      name: new FormControl('', [ Validators.required ]),
+      name: new FormControl('', [Validators.required]),
       information: new FormControl(''),
       continuation: new FormControl(false),
-      inventory: new FormArray([])
+      inventory: this.inv
     });
 
-    this.inv = this.formGroup.get("inventory") as FormArray
+    this.inv = this.formGroup.get("inventory") as FormArray<any>;
   }
 
   filterClient(event: any) {
@@ -75,29 +80,29 @@ export class NewSessionComponent {
 
   // TODO: TEMP CODE
   updateClientInfo() {
-    console.log('[this.formGroup]', this.formGroup.value)
+    console.log('[this.formGroup]', this.formGroup.value);
   }
 
   selectClient(event: any) {
     this.getUserInfoEl().forEach((el) => {
-      el.classList.remove('toggle-user-complement-info')
-    })
+      el.classList.remove('toggle-user-complement-info');
+    });
 
     this.formGroup.patchValue({
       'information': event.information
-    })
+    });
   }
 
   unselectClient() {
     this.getUserInfoEl().forEach((el) => {
-      el.classList.add('toggle-user-complement-info')
-    })
+      el.classList.add('toggle-user-complement-info');
+    });
 
     this.formGroup.patchValue({
       'name': '',
       'information': '',
       'continuation': false
-    })
+    });
   }
 
   getUserInfoEl() {
@@ -105,32 +110,64 @@ export class NewSessionComponent {
       document.getElementsByClassName(
         'user-complement-info'
       ) as HTMLCollectionOf<HTMLElement>
-    )
+    );
 
-    return el
+    return el;
   }
 
   addMaterial(id: number, value2: any, material: string) {
-    console.log('[event]', id, value2, material)
+    const inventoryArray = this.formGroup.get('inventory') as FormArray<any>;
 
-    const control = new FormControl({
-      '_id': id,
-      'value': value2
-    })
+    // Find the item in the inventory array
+    const itemIndex = inventoryArray.controls.findIndex((control) => {
+      return control.value._id === id;
+    });
 
-    // After added one
-    this.formGroup.value.inventory.find((el: any) => {
-      if (el._id == id) {
-        el.value = value2
-        console.log('[E IGUAL]', el.value)
-      }
+    const maxQuantity = this.getMaxQuantity(id);
 
-      return
-    })
-
-    // Add first element at list
-    this.inv.push(control)
-
+    if (itemIndex !== -1) {
+      // Update the quantity of the existing item
+      const item = inventoryArray.at(itemIndex);
+      const currentQuantity = item.value.quantity;
+      const updatedQuantity = Math.min(maxQuantity, value2);
+      item.patchValue({ quantity: updatedQuantity });
+      console.log(`Current quantity of ${material}: ${updatedQuantity}`);
+    } else {
+      // Add a new item to the inventory array
+      const control = new FormGroup({
+        _id: new FormControl(id),
+        quantity: new FormControl(Math.min(maxQuantity, value2))
+      });
+      inventoryArray.push(control);
+      console.log(`Current quantity of ${material}: ${control.value.quantity}`);
+    }
   }
 
+  getMaxQuantity(id: number): number {
+    const items = (this.formGroup.get('inventory') as FormArray<any>).controls;
+    let totalQuantity = 0;
+
+    for (const item of items) {
+      if (item.value._id === id) {
+        totalQuantity += item.value.quantity;
+      }
+    }
+
+    const selectedItem = this.inventoryList.find((item) => item._id === id);
+    return selectedItem ? selectedItem.quantity : totalQuantity;
+  }
+
+  startSession() {
+    // Get the selected client
+    const selectedClient = this.formGroup.value.name;
+
+    // Create a deep copy of the inventory array to pass to the modal
+    const inventoryCopy = JSON.parse(JSON.stringify(this.formGroup.value.inventory));
+
+    // Open the modal with the selected client name and inventory copy
+    // ... (code to open modal)
+
+    console.log('Selected Client:', selectedClient);
+    console.log('Inventory Copy:', inventoryCopy);
+  }
 }
