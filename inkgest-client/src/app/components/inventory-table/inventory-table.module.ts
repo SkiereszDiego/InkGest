13ebe7bf47a61ebd4efcfff23f0ee0f699d6d26b
diego skieresz <diegoskieresz@gmail.com>
2023-07-01 15:50:45 -0300

    Removed product mock services

--- inkgest-client/src/app/components/inventory-table/inventory-table.module.ts
+++ inkgest-client/src/app/components/inventory-table/inventory-table.module.ts
@@ -3,7 +3,6 @@ import { CommonModule, DatePipe } from '@angular/common';
 import { InventoryTableComponent } from './inventory-table.component';
 import { TableModule } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
-import { ProductService } from '../../shared/services/product.service';
 import { FormsModule } from '@angular/forms';
 import { CustomButtonModule } from "../custom-button/custom-button.module";
 import { InputNumberModule } from 'primeng/inputnumber';
@@ -11,6 +10,7 @@ import { ButtonModule } from 'primeng/button';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToastModule } from 'primeng/toast';
 import { DialogModule } from 'primeng/dialog';
+import { InventoryService } from 'src/app/shared/services/inventory.service';
 
 @NgModule({
     declarations: [
@@ -20,7 +20,7 @@ import { DialogModule } from 'primeng/dialog';
         InventoryTableComponent
     ],
     providers: [
-        ProductService,
+        InventoryService,
         DatePipe
     ],
     imports: [
